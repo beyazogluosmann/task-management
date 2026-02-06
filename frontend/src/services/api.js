2216e898331501fcc987670d2f94f603cb5c6095
beyazogluosmann <byzgluosmann@gmail.com>
2026-02-06 17:28:40 +0300

    fix: update API URL to use HTTPS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://task-management-rxoy.onrender.com/api' || 'http://localhost:5000/api';
+const API_URL = 'https://task-management-rxoy.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
